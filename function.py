638f954ee24f8150f1c06083851a6fc89f872dba
Evgeniya <kotenok.evgeniya@gmail.com>
2023-04-06 20:30:30 +0300

    добавила первое задание

--- function.py
+++ function.py
@@ -1,5 +1,16 @@
-def rhu_y(x,y):#Критерий нашей проверки (7) в статье 02,04,2023
-    rhu_y = abs(x - y)
+import numpy as np
 
+x = [3, 5, 6, 8]
+y = [2, 5 ,8 ,1]
+
+x = np.array(x)
+y = np.array(y)
+def rhu_y(x,y):#Критерий нашей проверки (7) в статье 02,04,2023
+    rhu_y = max(abs(x - y))
     return rhu_y
-print(rhu_y(3,5))
+
+
+rhu_y = max(abs(x - y))
+print(rhu_y)
+
+
