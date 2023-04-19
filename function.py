9f96233cf58e6de342531edbc6a1f7e08d2babe2
Evgeniya <kotenok.evgeniya@gmail.com>
2023-04-19 20:32:42 +0300

    добавила первое задание

--- function.py
+++ function.py
@@ -28,15 +28,36 @@ print(rhu_x(x,y))
 # y - бюджет( задаем сами )
 # a - критерии модели, прописываем вручную
 # b - критерии модели, прописываем вручную
+
+p = 100
+n = 10
+y = 10000
+a = 1000
+b = 1
+def summ_Di(y, p):
+    result = 0
+    for number in range(n-1):
+        result = result + (y * p)
+        return result
+
+print(summ_Di(y,p))
 def Di(p):
-    Di = 1 / (n * p) * (y * p) # А. В. Арутюнов, Н. Г. Павлова, А. А. Шананин 11 стр
+    Di = 1 / (n * p) * summ_Di(y, p) # А. В. Арутюнов, Н. Г. Павлова, А. А. Шананин 11 стр
     return Di
 print(Di(p))
 
+def summ_Si(p):
+    result = 0
+    for number in range(n - 1):
+        result = result + (p ** 2)
+        return result
+print(summ_Si(p))
 def Si(p):
-    Si = (((a + (b ** 2)) * p)/(b * ((a**2 + b**2)**0.5))) * ((p**2)**(-1/2)) - 1/n**0.5 # А. В. Арутюнов, Н. Г. Павлова, А. А. Шананин 11 стр
+    Si = (((a + (b ** 2)) * p)/(b * ((a**2 + b**2)**0.5))) * (summ_Si(p)**(-1/2)) - 1/n**0.5 # А. В. Арутюнов, Н. Г. Павлова, А. А. Шананин 11 стр
     return Si
 print(Si(p))
+
+
 # поправить формулы с учетом сумм, посмотреть на формулу
 
 
