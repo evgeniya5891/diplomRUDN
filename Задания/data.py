0bf67c57b75ea35c0110b8dedd22aca56c7e2f57
Evgeniya <kotenok.evgeniya@gmail.com>
2023-05-30 19:33:40 +0300

    добавила первое задание

---
+++ Задания/data.py
@@ -0,0 +1,136 @@
+
+
+import numpy as np
+
+c1 = [1, 1]
+c2 = [5, 5]
+n = 2
+gamma = [0.01 , 0.01]
+a = 1
+b = 1
+
+c1 = np.array(c1)
+c2 = np.array(c2)
+
+gamma = np.array(gamma)
+
+def f1():
+    list1 = []
+    for number in range(0,n):
+        a = (c2[number] -c1[number]) / c1[number]
+        list1.append(a)
+    list1 = np.array(list1)
+    return list1
+# print(f1())
+
+def f2():
+    list2 = []
+    for number in range(0,n):
+        a = c1[number] / (c2[number] -c1[number])
+        list2.append(a)
+    list2 = np.array(list2)
+    return list2
+# print(f2())
+
+def f3():
+    list2 = []
+    for number in range(0,n):
+        a = c1[number] * (c2[number] - c1[number])**float(-1) * c2[number]
+        list2.append(a)
+    list2 = np.array(list2)
+    return list2
+# print(f3())
+
+def f4():
+    list2 = []
+    for number in range(0,n):
+        a = c1[number] + c2[number]
+        list2.append(a)
+    list2 = np.array(list2)
+    return list2
+#print(f4())
+
+def f5():
+    list2 = []
+    for number in range(0,n):
+        a = c2[number] - c1[number]
+        list2.append(a)
+    list2 = np.array(list2)
+    return list2
+# print(f5())
+
+def f6():
+    result = 0
+    for number in range(0,n):
+        a = min(abs(gamma - ((n * c2)**float(-1) * np.dot(gamma,c1))))
+        result = result + a
+    return result
+# print( f6())
+
+def f7():
+    a = (n + 1) / (2 * (b**2)) * min(f1())
+    return a
+print(f7())
+def f8():
+    a = (n + 1) / n * max(c2) * max(c1**float(-2)) * (max(f2()))**float(-1) * np.dot(gamma,c1)
+    return a
+print(f8())
+
+
+def GAMMA():
+    list2 = []
+    for number in range(1,n+1):
+        w = abs(((n*f4()[number-1])**float(-1) * np.dot(gamma,f4())) - ((((a + b**2) * f4()[number-1] )/ (b * (a**2 + b**2)**(1/2))) * (np.dot(f4(),f4()))**float(-1/2)) + n**float(-1/2))
+        list2.append(w)
+    list2 = np.array(list2)
+    return max(list2)
+
+print(GAMMA())
+
+def ALFA():
+    w =  2 * n * (max(f3() * np.dot(gamma,c2))**float(-1)) * max(f2()) * f6() * max(c1**2)*((np.dot(gamma,c2))**float(-2))
+    return w
+
+print(ALFA())
+
+def BETA():
+    a = f8() - f7()
+    return a
+print(BETA())
+
+def condition():
+    if GAMMA() < ALFA() - BETA() and f7() <  ALFA() - f8():
+        return True #'модель удовлетворяет условиям'
+    else:
+        return False #'модель НЕ удовлетворяет условиям'
+print(condition())
+
+
+
+
+# функции f1, f2, f3, f4, f5, f6, f7, f8, GAMMA, ALFA, BETA, condition определены выше
+
+n = 2
+
+while True:
+    c1 = np.random.uniform(low=0.1, high=10.0, size=n) # генерируем случайные числа в диапазоне от 0.1 до 10.0
+    c2 = np.random.uniform(low=0.1, high=10.0, size=n)
+    gamma = np.random.uniform(low=0.01, high=1.0, size=n)
+    alpha = f7() + np.random.uniform(low=0.1, high=1.0)
+    beta = f8() - np.random.uniform(low=0.1, high=1.0)
+    gamma_max = GAMMA()
+    alpha_val = ALFA()
+    beta_val = BETA()
+
+    if gamma_max < alpha_val - beta_val and alpha < alpha_val - beta:
+        # записываем найденный набор в файл
+        with open('solution.txt', 'w') as f:
+            f.write(f'c1: {c1}\n')
+            f.write(f'c2: {c2}\n')
+            f.write(f'gamma: {gamma}\n')
+            f.write(f'alpha: {alpha}\n')
+            f.write(f'beta: {beta}\n')
+            f.write(f'gamma_max: {gamma_max}\n')
+            f.write(f'alpha_val: {alpha_val}\n')
+            f.write(f'beta_val: {beta_val}\n')
+        break
